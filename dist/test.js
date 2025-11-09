b27d87ba3f431c11c3c0a6d10b17b74c2773a714
sanjidarimi023 <sanjidarimi023@gmail.com>
2025-11-09 22:10:36 +0600

    added ts config file

---
+++ dist/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const newLanguage = "now i want to learn typescipt";
+console.log(newLanguage);
+//# sourceMappingURL=test.js.map
