4ac1374b953606f543025f07a8f816002a981bbd
sanjidarimi023 <sanjidarimi023@gmail.com>
2025-11-13 23:20:17 +0600

    learn the class concept

---
+++ src/class.ts
@@ -0,0 +1,12 @@
+//oop -> class -> object
+
+class Student{
+    name: string;
+    department : string;
+    roll : number;
+    constructor(name:string, department:string, roll:number){
+        this.name= name,
+        this.department=department,
+        this.roll=roll
+    }
+}
